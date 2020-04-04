2b64106a51bd0ef7a399d40db1b1a53fe4c21a8c
Lumbo1379 <36245864+Lumbo1379@users.noreply.github.com>
2020-04-04 01:19:29 +0100

    Vacuum
    Objects shrink and if fall out range balloon back to og scale

---
+++ Assets/GetSucked.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSucked : MonoBehaviour
+{
+    public Transform Target { get; set; }
+    public float RangeToScaleDown { get; set; }
+
+    [Range(0, 10)] public float ShrinkSpeed;
+    [Range(0, 10)] public float BalloonSpeed;
+
+    private Rigidbody _rb;
+    private Vector3 _originalScale;
+    private float _t;
+
+    private void Awake()
+    {
+        _originalScale = transform.localScale;
+    }
+
+    private void OnEnable()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+        _rb.velocity = Vector3.zero;
+        _t = 0;
+    }
+
+    private void Update()
+    {
+        _t += Time.deltaTime;
+
+        if (_t < 1)
+        {
+            if (!_rb.useGravity)
+            {
+                var distance = Vector3.Distance(transform.position, Target.position);
+                float scaleMultiplier = distance / RangeToScaleDown;
+                scaleMultiplier = Mathf.Clamp(scaleMultiplier, 0, 1);
+                scaleMultiplier = Mathf.Lerp(_originalScale.x, scaleMultiplier, _t * ShrinkSpeed);
+                transform.localScale = _originalScale * scaleMultiplier;
+            }
+            else
+            {
+                float scaleMultiplier = Mathf.Lerp(transform.localScale.x, _originalScale.x, _t * BalloonSpeed);
+                transform.localScale = _originalScale * scaleMultiplier;
+
+                if (Math.Abs(transform.localScale.x - _originalScale.x) < 0.01)
+                    enabled = false;
+            }
+        }
+    }
+
+    public void Release()
+    {
+        _rb.useGravity = true;
+        _t = 0;
+    }
+}
---
+++ Assets/VacuumSuck.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class VacuumSuck : MonoBehaviour
+{
+    [Range(0, 100)] public float AttractPower;
+    [Range(0, 100)] public float Range;
+    [Range(0, 3)] public float AttractArea;
+    public Transform SuckOrigin;
+    public LayerMask SuckMask;
+
+    private Camera _camera;
+    private List<GameObject> _currentAttractedObjects;
+    private List<GameObject> _attractedObjectsInFrame;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+        _currentAttractedObjects = new List<GameObject>();
+        _attractedObjectsInFrame = new List<GameObject>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+            DrawRay();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            ReleaseObjects();
+    }
+
+    private void DrawRay()
+    {
+        RaycastHit hit;
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Range))
+        {
+            foreach (var collision in ScanForColliders(hit.point))
+            {
+                if (((1 << collision.gameObject.layer) & SuckMask) != 0)
+                {
+                    _attractedObjectsInFrame.Add(collision.gameObject);
+
+                    AttractObject(hit.point, collision.gameObject, hit.distance);
+
+                    var getSucked = collision.gameObject.GetComponent<GetSucked>();
+                    if (!getSucked.isActiveAndEnabled)
+                    {
+                        _currentAttractedObjects.Add(collision.gameObject);
+
+                        getSucked.Target = SuckOrigin;
+                        getSucked.RangeToScaleDown = Range;
+                        getSucked.enabled = true;
+                    }
+                }
+            }
+
+            CheckIfObjectsLeftAttractRadius();
+        }
+    }
+
+    private void AttractObject(Vector3 hitPosition, GameObject obj, float distanceAway)
+    {
+        var fallOff = (Range - distanceAway) / Range;
+        var rb = obj.GetComponent<Rigidbody>();
+        var direction = SuckOrigin.position - obj.transform.position;
+
+        rb.AddForce(direction * AttractPower * fallOff * Time.deltaTime);
+    }
+
+    private void CheckIfObjectsLeftAttractRadius()
+    {
+        foreach (var obj in _currentAttractedObjects.ToList()) // Create copy as modifying when iterating on it
+        {
+            if (!_attractedObjectsInFrame.Contains(obj))
+            {
+                obj.GetComponent<GetSucked>().Release();
+                _currentAttractedObjects.Remove(obj);
+            }
+        }
+
+        _attractedObjectsInFrame.Clear();
+    }
+
+    private void ReleaseObjects()
+    {
+        foreach (var currentAttractedObject in _currentAttractedObjects)
+            currentAttractedObject.GetComponent<GetSucked>().Release();
+
+        _currentAttractedObjects.Clear();
+    }
+
+    private Collider[] ScanForColliders(Vector3 hit)
+    {
+        return Physics.OverlapSphere(hit, AttractArea);
+    }
+}
